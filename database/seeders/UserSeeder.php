f9c69f0d0d79a365ff08ef22c69297460992f30e
FaniRamadhanty <fani_173@smkassalaambandung.sch.id>
2021-12-23 10:45:03 +0700

    Belajar Auth

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        //membuat sample role
+        $adminRole = new Role();
+        $adminRole->name = "admin";
+        $adminRole->display_name = "Administrator";
+        $adminRole->save();
+        
+  
+        $memberRole = new Role();
+        $memberRole->name = "member";
+        $memberRole->display_name = "Member";
+        $memberRole->save();
+
+        //membuat sample admin
+        $admin = new User();
+        $admin->name = 'admin pkl';
+        $admin->email = 'admin@gmail.com';
+        $admin->password = Hash::make('12345678');
+        $admin->save();
+        $admin->attachRole($adminRole);
+    
+        $member = new User();
+        $member->name = 'Member Users';
+        $member->email = 'member@gmail.com';
+        $member->password = bcrypt('12345678');
+        $member->save();
+        $member->attachRole($memberRole);
+    }
+}
